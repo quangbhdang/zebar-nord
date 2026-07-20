8154c9bc6cb185221a306ef4f15e98a2921d8da4
quangbhdang <anda32664@gmail.com>
2026-07-20 09:36:40 +0700

    feat(direction): ensure lighthouse Nerd Font icon logo is always displayed

--- src/components/bar/direction.tsx
+++ src/components/bar/direction.tsx
@@ -1,28 +1,35 @@
 import { cn } from "../../lib/utils";
-import { createEffect, createSignal, Show } from "solid-js";
 import { useProviders } from "../../lib/providers-context";
-import * as zebar from "zebar";
 
 function Direction() {
   const { glazewm } = useProviders();
-  const [glazewmSig, setGlazewmSig] = createSignal<
-    zebar.GlazeWmOutput | undefined
-  >(undefined);
+
+  const isVertical = () => glazewm()?.tilingDirection === "vertical";
+
+  const handleClick = () => {
+    if (glazewm()?.tilingDirection) {
+      glazewm()?.runCommand("toggle-tiling-direction");
+    }
+  };
 
   return (
-    <Show when={!!glazewm()?.tilingDirection}>
-      <button
-        class={cn(
-          "h-8 w-8 flex items-center justify-center text-[var(--icon)] bg-[var(--icon)]/10 rounded-full p-1 transition-all duration-300",
-          {
-            "rotate-90": glazewm()?.tilingDirection === "vertical",
-          }
-        )}
-        onClick={() => glazewm()?.runCommand(`toggle-tiling-direction`)}
-      >
-        <i class="nf nf-md-lighthouse text-lg"></i>
-      </button>
-    </Show>
+    <button
+      class={cn(
+        "h-8 w-8 flex items-center justify-center text-[var(--icon)] bg-[var(--icon)]/10 rounded-full p-1 transition-all duration-300 select-none",
+        {
+          "rotate-90": isVertical(),
+          "cursor-pointer": Boolean(glazewm()?.tilingDirection),
+        }
+      )}
+      onClick={handleClick}
+      title={
+        glazewm()?.tilingDirection
+          ? `Toggle Tiling Direction (${glazewm()?.tilingDirection})`
+          : "Zebar Nord"
+      }
+    >
+      <i class="nf nf-md-lighthouse text-lg"></i>
+    </button>
   );
 }
 
